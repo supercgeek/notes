eab4d6d715effee24bf86d96c1fc6444298ae756
afratila <alexfr425@gmail.com>
2016-02-06 14:32:55 -0500

    Initial Commit

---
+++ notes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  notes
+//
+//  Created by Alex Fratila on 2/6/16.
+//  Copyright © 2016 Alex Fratila. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
